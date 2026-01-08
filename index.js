138f672c429bb84ba8618dc050ae20e2e3ce18e6
CodError404 <160505130+TheGlauberShow@users.noreply.github.com>
2026-01-08 12:42:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-import express from "express";
-import fetch from "node-fetch";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.get("/search", async (req, res) => {
     const q = req.query.q || "";
+    const cursor = req.query.cursor || "";
 
     const url =
       `https://catalog.roblox.com/v1/search/items/details` +
-      `?Keyword=${encodeURIComponent(q)}&Limit=30`;
+      `?Keyword=${encodeURIComponent(q)}` +
+      `&Limit=30` +
+      `&Cursor=${cursor}`;
 
     const r = await fetch(url);
     const data = await r.json();
 
-    res.json(data.data.map(item => ({
-        id: item.id,
-        name: item.name,
-        price: item.price,
-        creator: item.creatorName,
-        description: item.description,
-        thumbnail:
-          `https://www.roblox.com/asset-thumbnail/image?assetId=${item.id}&width=420&height=420`
-    })));
-});
-
-app.listen(PORT, () => {
-    console.log("Servidor online na porta", PORT);
+    res.json({
+        items: data.data.map(item => ({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            creator: item.creatorName,
+            description: item.description,
+            thumbnail:
+              `https://www.roblox.com/asset-thumbnail/image?assetId=${item.id}&width=420&height=420`
+        })),
+        nextCursor: data.nextPageCursor
+    });
 });
